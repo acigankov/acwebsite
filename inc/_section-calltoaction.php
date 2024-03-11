01954070adc0c390f3c1d56dfbfa573378537ffc
acigankov@inbox.ru <diiPs56uaY>
2024-03-11 22:49:54 +0300

    Правки текста

--- inc/_section-calltoaction.php
+++ inc/_section-calltoaction.php
@@ -6,7 +6,7 @@
             <div class="col-xl-8 col-lg-9">
                 <div class="call-action-content text-center">
                     <h2 class="action-title">Есть проект или идея? </h2>
-                    <p>Если у Вас есть проект в разработке или идея для проекта, давайте подумааем и осуществим вместе!</p>
+                    <p>Если у Вас есть проект в разработке или идея для проекта, давайте подумаем и осуществим вместе!</p>
                     <ul>
                         <li><a class="main-btn custom" href="<?= get_template_directory_uri() . '/assets/docs/cv-acigankov.pdf'?>">Скачать резюме</a></li>
                         <li><a class="main-btn custom-2" href="#contact">Написать</a></li>
